f217de36866ef4d85a6c42018dd010e030d90abb
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-18 17:06:18 -0600

    put platform buttons into modal on repeat

---
+++ public/scripts/controllers/modalControllers.js
@@ -0,0 +1,85 @@
+//Modal controllers
+myApp.controller('ModalDemoCtrl', ['$uibModal', '$log', '$document', function($uibModal, $log, $document) {
+  console.log('in ModalDemoCtrl');
+  var $ctrl = this;
+  $ctrl.items = ['PayPal', 'Razoo', 'YouCaring'];
+
+  $ctrl.animationsEnabled = true;
+
+  //handle modal open
+  $ctrl.open = function (size, parentSelector) {
+    var parentElem = parentSelector ?
+      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+      size: size,
+      appendTo: parentElem,
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+    //handles modal cancel - logs the time of cancellation
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $ctrl.toggleAnimation = function () {
+    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  };
+
+}]); // end ModalDemoCtrl
+
+myApp.controller('ModalInstanceCtrl', ['$uibModalInstance', 'items', function($uibModalInstance, items) {
+  console.log('in ModalInstanceCtrl');
+  var $ctrl = this;
+  $ctrl.items = items;
+  $ctrl.selected = {
+    item: $ctrl.items[0]
+  };
+
+  $ctrl.ok = function () {
+    $uibModalInstance.close($ctrl.selected.item);
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+}]); // end ModalInstanceCtrl
+
+// The close and dismiss bindings are from $uibModalInstance.
+angular.module('myApp').component('modalComponent', {
+  templateUrl: 'myModalContent.html',
+  bindings: {
+    resolve: '<',
+    close: '&',
+    dismiss: '&'
+  },
+  controller: function () {
+    var $ctrl = this;
+
+    $ctrl.$onInit = function () {
+      $ctrl.items = $ctrl.resolve.items;
+      $ctrl.selected = {
+        item: $ctrl.items[0]
+      };
+    };
+
+    $ctrl.ok = function () {
+      $ctrl.close({$value: $ctrl.selected.item});
+    };
+
+    $ctrl.cancel = function () {
+      $ctrl.dismiss({$value: 'cancel'});
+    };
+  }
+}); // end modalComponent
