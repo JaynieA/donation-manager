34bb8075b174064f34d383de8dbc8cd298669d98
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-19 08:35:26 -0600

    simplified modal controller

--- public/scripts/controllers/modalControllers.js
+++ public/scripts/controllers/modalControllers.js
@@ -1,85 +1,31 @@
-//Modal controllers
-myApp.controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
-  var $ctrl = this;
-  $ctrl.platforms = ['Paypal', 'Razoo', 'WePay'];
-
-  //allow fade/slide-in
-  $ctrl.animationsEnabled = true;
+//ModalCtrl
+myApp.controller('ModalCtrl', ['$scope','$uibModal',function ($scope, $uibModal) {
 
   //open the modal
-  $ctrl.open = function (size, parentSelector) {
+  $scope.open = function (size) {
+    console.log('opening pop up');
     var modalInstance = $uibModal.open({
-      animation: $ctrl.animationsEnabled,
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      controllerAs: '$ctrl',
+      templateUrl: 'uploadReportsModal.html',
+      controller: 'ModalInstanceController',
       size: size,
-      resolve: {
-        platforms: function () {
-          return $ctrl.platforms;
-        }
-      }
-    });
+    }); // end modalInstance
+  }; // end open
 
-    modalInstance.result.then(function (selectedPlatform) {
-      $ctrl.selected = selectedPlatform;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
+}]); // end ModalCtrl
 
-  //enable animations
-  $ctrl.toggleAnimation = function () {
-    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
-  };
-});
+//In ModalInstanceController, we have passed $modalInstance
+//which is the instance of modal return by the open() function.
+//We need to pass this instance because dismiss is the property of
+//this instance object which is used to close the modal.
 
-// $uibModalInstance represents a modal window (instance) dependency.
-// It is not the same as the $uibModal service used above.
-
-myApp.controller('ModalInstanceCtrl', function ($uibModalInstance, platforms) {
-  var $ctrl = this;
-  $ctrl.platforms = platforms;
-  $ctrl.selected = {
-    platform: $ctrl.platforms[0]
-  };
-
-  $ctrl.ok = function () {
-    $uibModalInstance.close($ctrl.selected.platform);
-  };
-
-  $ctrl.cancel = function () {
+//ModalInstanceController
+myApp.controller('ModalInstanceController', ['$scope','$uibModalInstance',function ($scope, $uibModalInstance) {
+  $scope.close = function () {
     $uibModalInstance.dismiss('cancel');
-  };
-});
+  }; // end close
 
-// Please note that the close and dismiss bindings are from $uibModalInstance.
-
-myApp.component('modalComponent', {
-  templateUrl: 'myModalContent.html',
-  bindings: {
-    resolve: '<',
-    close: '&',
-    dismiss: '&'
-  },
-  controller: function () {
-    var $ctrl = this;
-
-    $ctrl.$onInit = function () {
-      $ctrl.platforms = $ctrl.resolve.platforms;
-      $ctrl.selected = {
-        platform: $ctrl.platforms[0]
-      };
-    };
-
-    $ctrl.ok = function () {
-      $ctrl.close({$value: $ctrl.selected.platform});
-    };
-
-    $ctrl.cancel = function () {
-      $ctrl.dismiss({$value: 'cancel'});
-    };
-  }
-});
+  $scope.save = function () {
+    console.log('save modal results');
+    $uibModalInstance.close();
+  }; // end close
+}]); // end ModalInstanceController
