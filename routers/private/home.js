926c59c503a920d102ff6e3132621fa5b62e4f4a
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-20 15:37:10 -0600

    clean up /private/home router

--- routers/private/home.js
+++ routers/private/home.js
@@ -1,29 +1,24 @@
 /**
- * Handles requests for Google calendar data.
  * @module private/calendar
  */
 var express = require('express');
 var router = express.Router();
 var Donation = require('../../models/donation');
-/**
- * GET /private/home
- *
- * @todo Get some data from the Google API. Call the API using the token
- * saved to the user.
- * @see {@link https://www.npmjs.com/package/google-calendar}
- * @see {@link https://developers.google.com/google-apps/calendar/v3/reference/#Calendars}
- *
- */
+
+// GET /private/home
 router.get('/', function (req, res) {
   res.send({ authStatus: true });
 });
 
+//POST /private/home
 router.post('/', function(req,res) {
   console.log(req.body.donations);
   var donations = req.body.donations;
   //create an entry in the database for each donation
   for (var i = 0; i < donations.length; i++) {
+    //use donation model to create new donation document
     var newDonation = new Donation(donations[i]);
+    //save the new donation in the database
     newDonation.save(function(err){
       if(err){
         console.log(err);
