5b3badd4900a7fdc155f10a61f708cf36ec7eec7
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-20 16:08:25 -0600

    convert dashboardController to use

--- public/scripts/controllers/dashboardController.js
+++ public/scripts/controllers/dashboardController.js
@@ -1,18 +1,17 @@
 // //Dashboard Controller
 
-myApp.controller('DashboardController', function ($http) {
-  console.log('loaded DashboardController');
-  var _this = this;
-  _this.data = '';
+myApp.controller('DashboardController', ['$scope', '$http', function($scope, $http) {
+  console.log('in DashboardController');
   //get authentication that user is logged in and has admin status
   $http.get('/private/dashboard')
     .then(function (response) {
-      if (response.data.err) {
-        console.log('Sorry, you are not logged in.');
-        _this.data = false;
-      } else {
-        _this.data = response.data.authStatus;
-        console.log('DC. You are logged in:', response.data.authStatus);
-      }
-    });
-}); // end DashboardController
+    if (response.data.err) {
+      console.log('Sorry, you are not logged in.');
+      $scope.data = false;
+    } else {
+      $scope.data = response.data.authStatus;
+      console.log('DC. You are logged in:', response.data.authStatus);
+    } // end else
+  }); // end $http
+
+}]); // end DashboardController
