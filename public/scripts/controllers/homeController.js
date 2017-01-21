4e0b27db5ca405352b1e7f9da428a54c91bf1f1d
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-20 22:31:51 -0600

    redirect view location to login page if auth status is not true

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,15 +1,18 @@
 //Home Controller
-myApp.controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+myApp.controller('HomeController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
   console.log('loaded HomeController');
 
   $scope.data = '';
   //check if the user is allowed to see this page
   $http.get('/private/home')
     .then(function (response) {
-      if (response.data.err) {
+      //if the authStatus is false or undefined...
+      if (response.data.authStatus === undefined || response.data.authStatus === false) {
         //if not logged in/authorized as admin set data to false
         console.log('Sorry, you are not logged in.');
         $scope.data = false;
+        //redirect to the login page
+        $location.path("/#!/login");
       } else {
         //else, get server response of true
         $scope.data = response.data.authStatus;
--- public/scripts/controllers/templatesController.js
+++ public/scripts/controllers/templatesController.js
@@ -1,15 +1,18 @@
 //Templates Controller
 
-myApp.controller('TemplatesController', function ($http) {
+myApp.controller('TemplatesController', function ($http, $location) {
   console.log('loaded TemplatesController');
   var _this = this;
   _this.data = '';
   //get authentication that user is logged in and has admin status
   $http.get('/private/templates')
     .then(function (response) {
-      if (response.data.err) {
+      //if the authStatus is false or undefined...
+      if (response.data.authStatus === undefined || response.data.authStatus === false) {
         console.log('Sorry, you are not logged in.');
         _this.data = false;
+        //redirect to the login page
+        $location.path("/#!/login");
       } else {
         _this.data = response.data.authStatus;
         console.log('TC. You are logged in:', response.data.authStatus);
