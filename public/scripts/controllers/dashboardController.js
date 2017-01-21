13936def3923c2d79c9a2a5a0691b6f0348f1f08
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-20 22:27:25 -0600

    redirect dashboard controller with

--- public/scripts/controllers/dashboardController.js
+++ public/scripts/controllers/dashboardController.js
@@ -1,19 +1,23 @@
 // //Dashboard Controller
 
-myApp.controller('DashboardController', ['$scope', '$http', function($scope, $http) {
+myApp.controller('DashboardController', ['$scope', '$http','$location', function($scope, $http, $location) {
   console.log('in DashboardController');
 
   //get authentication that user is logged in and has admin status
   $http.get('/private/dashboard')
     .then(function (response) {
-    if (response.data.err) {
+    //if the authStatus is false or undefined...
+    if (response.data.authStatus === undefined || response.data.authStatus === false) {
       console.log('Sorry, you are not logged in.');
       $scope.data = false;
+      //redirect to the login page
+      $location.path("/#!/login");
     } else {
       $scope.data = response.data.authStatus;
       console.log('DC. You are logged in:', response.data.authStatus);
       $scope.donations = response.data.donations;
     } // end else
+
   }); // end $http
 
 }]); // end DashboardController
