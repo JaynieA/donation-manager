f66fab5cbec1e7b2c856309acb40c4e29e49d5ac
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-22 20:25:13 -0600

    creating pdf with lorem ipsum, contains bulleted list

--- utils/generatePDFDocument.js
+++ utils/generatePDFDocument.js
@@ -2,22 +2,37 @@
 var PDF = require('pdfkit');
 var fs = require('fs');
 
-//TODO: use Router to export this as a function...
-//TODO: get the template text to send and be included here
+//TODO: use Router to export this as a functionality...
 
 //define text to be contained in the pdf document
-var text = 'HOORAY I MADE SOMETHING!' + new Date();
+var greeting = 'Dear Spot\s Last Stop Canine Rescue Supporter,';
+//TODO: get the template text to send and be included here
+var lorem = "Wafer powder ice cream sweet. Chocolate cake cake bear claw cupcake powder carrot cake. Powder pie wafer biscuit fruitcake sugar plum. Cheesecake sweet lollipop brownie tootsie roll croissant fruitcake. Jelly-o pastry sesame snaps pie caramels. Pie liquorice brownie tart halvah. Fruitcake brownie jelly icing jujubes cotton candy gingerbread. Danish sesame snaps chocolate bear claw cake bear claw pastry. Apple pie toffee danish apple pie danish. Icing pastry jelly. Biscuit jelly topping. Chocolate dessert dragée.";
+var date = new Date();
+var signatureOne = "Casey and Grant Adams";
+var signatureTwo = 'Founder and Directors';
+
 //creating a new PDF object
 doc = new PDF();
+
 //creating a write stream to write the content on the file system
 doc.pipe( fs.createWriteStream( 'pdfTemplates/NewDoc.pdf' ));
 
-
+//Set the font size
+doc.fontSize(14);
 //Add the image
-doc.image('pdfTemplates/banner.png', 165, 30, { height: 75, width: 275 } );
+doc.image('pdfTemplates/banner.png', 165, 50, { height: 75, width: 275 } );
 //adding the text to be written,
-doc.text( text, 100, 100 ); // more things can be added here including new pages
-
+doc.text( greeting, 100, 150);
+doc.moveDown();
+doc.text( lorem );
+doc.moveDown();
+doc.list(['one', 'two', 'three']);
+doc.moveDown();
+doc.text( date );
+doc.moveDown();
+doc.text( signatureOne );
+doc.text( signatureTwo );
 
 //end the document writing.
 doc.end();
