f8e643ff2d04b541e4ae6107b8ecfb7e2d3fd426
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-24 16:46:30 -0600

    display default template on templates page

--- public/scripts/controllers/templatesController.js
+++ public/scripts/controllers/templatesController.js
@@ -21,30 +21,45 @@ myApp.controller('TemplatesController', ['$scope', '$http', '$location', functio
       }); // end $http
   }; // end getAuthStatus
 
+  var getTemplates = function() {
+    console.log('in getTemplates');
+    $http({
+      method: 'GET',
+      url: '/private/templates/default'
+    }).then(function(response) {
+      console.log(response.data.text);
+      //set the template email display
+      $scope.template.email = response.data.text;
+    }); // end $http
+  }; // end getTemplates
+
+  //TODO: change this to edit **email** template
+  // which means changing it to a put and editing the find() function on server....
   $scope.saveTemplate = function() {
     console.log('in saveTemplate', $scope.template.email);
     //assemble object to send
     var objectToSend = {
       type: 'email',
-      default: true,
+      default: false,
       text: $scope.template.email,
     }; // end objectToSend
     //post info to the server
-    $http({
-      method: 'POST',
-      url: '/private/templates',
-      data: objectToSend
-    }).then( function(response) {
-      console.log(response);
-    }); // end $http
+    // $http({
+    //   method: 'POST',
+    //   url: '/private/templates',
+    //   data: objectToSend
+    // }).then( function(response) {
+    //   console.log(response);
+    // }); // end $http
   }; // end saveTemplate
 
   var init = function() {
     console.log('in init');
     $scope.data = '';
     getAuthStatus();
+    getTemplates();
     $scope.template = {
-      email: "Email template stuff goes here"
+      //email: "Email template stuff goes here"
     };
   }; // end init
 
--- routers/private/templates.js
+++ routers/private/templates.js
@@ -7,13 +7,32 @@ var Template = require('../../models/template');
 /**
  * GET /private/templates
  */
+
 router.get('/', function (req, res) {
   res.send({ authStatus: true });
 });
 
+//GET default email template
+router.get('/default', function(req,res) {
+  Template.findOne({type: 'email', default: true}, function(err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(results);
+    } // end else
+  }); // end find
+}); // end get
+
 router.post('/', function(req,res) {
   console.log(req.body);
-  res.sendStatus(200);
+  var newTemplate = new Template(req.body);
+  newTemplate.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.sendStatus(201);
+    } // end else
+  }); // end save
 }); // end post
 
 module.exports = router;
