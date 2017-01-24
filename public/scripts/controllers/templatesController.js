48ac81e4d1d097c1076dfc19193ddd1edd66c5b1
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-24 14:56:13 -0600

    refactor templatesController to use

--- public/scripts/controllers/templatesController.js
+++ public/scripts/controllers/templatesController.js
@@ -1,21 +1,32 @@
 //Templates Controller
 
-myApp.controller('TemplatesController', function ($http, $location) {
+myApp.controller('TemplatesController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
   console.log('loaded TemplatesController');
-  var _this = this;
-  _this.data = '';
-  //get authentication that user is logged in and has admin status
-  $http.get('/private/templates')
-    .then(function (response) {
-      //if the authStatus is false or undefined...
-      if (response.data.authStatus === undefined || response.data.authStatus === false) {
-        console.log('Sorry, you are not logged in.');
-        _this.data = false;
-        //redirect to the login page
-        $location.path("/#!/login");
-      } else {
-        _this.data = response.data.authStatus;
-        console.log('TC. You are logged in:', response.data.authStatus);
-      }
-    });
-}); // end TemplatesController
+
+  var getAuthStatus = function() {
+    console.log('in getAuthStatus');
+    //get authentication that user is logged in and has admin status
+    $http.get('/private/templates')
+      .then(function (response) {
+        //if the authStatus is false or undefined...
+        if (response.data.authStatus === undefined || response.data.authStatus === false) {
+          console.log('Sorry, you are not logged in.');
+          $scope.data = false;
+          //redirect to the login page
+          $location.path("/#!/login");
+        } else {
+          $scope.data = response.data.authStatus;
+          console.log('TC. You are logged in:', response.data.authStatus);
+        } // end else
+      }); // end $http
+  }; // end getAuthStatus
+
+  var init = function() {
+    console.log('in init');
+    $scope.data = '';
+    getAuthStatus();
+  }; // end init
+
+  init();
+
+}]); // end TemplatesController
