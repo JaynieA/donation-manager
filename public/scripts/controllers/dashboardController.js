b51504a1d02b0bd32575e5a8d7d9909ecc04ab9f
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-24 09:25:36 -0600

    add angular-sanitize and ui-select

--- public/scripts/controllers/dashboardController.js
+++ public/scripts/controllers/dashboardController.js
@@ -132,6 +132,17 @@ myApp.controller('DashboardController', ['$scope', '$http','$location', function
     $scope.shouldFilter = true;
   }; // end enableFilters
 
+  $scope.itemArray = [
+        {id: 1, name: 'first'},
+        {id: 2, name: 'second'},
+        {id: 3, name: 'third'},
+        {id: 4, name: 'fourth'},
+        {id: 5, name: 'fifth'},
+    ];
+
+    $scope.selected = { value: $scope.itemArray[0] };
+
+
 
   $scope.thank = function(donationObject) {
     console.log('in thank');
