8829e31c538337cd5f9ec693279982fd21f272cb
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-02-01 20:21:33 -0600

    pull authFactory into usersController

--- client/controllers/usersController.js
+++ client/controllers/usersController.js
@@ -1,5 +1,34 @@
 //Users Controller
-myApp.controller('UsersController', function ($http) {
-  //if (verbose)
+myApp.controller('UsersController', ['$http', '$scope', '$location', 'AuthFactory', function($http, $scope, $location, AuthFactory) {
   console.log('loaded UsersController');
-}); // end ErrorController
+  //declare authFactory
+  var authFactory = AuthFactory;
+  var authorized = authFactory.checkLoggedIn();
+  //if (verbose)
+  console.log('AUTH in DC-->',authorized);
+
+  var confirmAuth = function() {
+    if (verbose) console.log('in confirmAuth');
+    //confirm that the user is authorized
+    if (authorized) {
+      //initialize the view
+      init();
+      //show the view to the user
+      $scope.data = authorized;
+      if (verbose) console.log('DC. You are logged in:', authorized);
+    } else {
+      if (verbose) console.log('Sorry, you are not logged in.');
+      //do not show the view
+      $scope.data = false;
+      //redirect to the login page
+      $location.path("/#!/login");
+    } // end else
+  }; // end
+
+  var init = function() {
+    console.log('in init');
+  }; // end init
+
+  confirmAuth();
+
+}]); // end UsersController
