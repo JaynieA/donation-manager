c999ef6ae3ba9556d4db7708714a68db932c55e5
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-03-03 12:46:25 -0600

    initialize dashboard filter to not thanked

--- client/controllers/dashboardController.js
+++ client/controllers/dashboardController.js
@@ -127,8 +127,8 @@ myApp.controller('DashboardController', ['AuthFactory', 'DonationFactory', '$sco
     $scope.thankSelected = { value: undefined };
     $scope.monthSelected = { value: undefined };
     $scope.yearSelected = { value: undefined };
-    //TODO: initiate thanked filter to 'Not Thanked' before deploy
-    //$scope.thankSelected = { value: {value: false, display: 'Not Thanked'} };
+    //Initiate thanked filter to 'Not Thanked'
+    $scope.thankSelected = { value: {value: false, display: 'Not Thanked'} };
   }; // end init
 
   var makeMonthsObject = function(monthsArray) {
